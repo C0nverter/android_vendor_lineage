LineageOS/android_vendor_cm

Disable LiveDisplay by default for ATV
This required adding an overlay directory for tv

Change-Id: I6e43463afb2c277520598e7b382a19dc33cf3b34

config/common_full_tv.mk
@@ -2,3 +2,5 @@
 $(call inherit-product, vendor/cm/config/common_full.mk)
 
 PRODUCT_PACKAGES += TvSettings
+
+DEVICE_PACKAGE_OVERLAYS += vendor/cm/overlay/tv

C0nverter/android_vendor_lineage

Change-Id: I66749522a62b6d94002a8a7c9f632d1b5f6a2600

config/common_full_tv.mk
@@ -2,3 +2,5 @@
 $(call inherit-product, vendor/lineage/config/common_full.mk)
 
 PRODUCT_PACKAGES += TvSettings
+
+DEVICE_PACKAGE_OVERLAYS += vendor/lineage/overlay/tv